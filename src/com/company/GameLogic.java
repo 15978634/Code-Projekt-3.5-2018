164ce523aa0ad1c6145abb2a8dc26eae2ea5b05b
AlexGit1 <alexanderwiesner@gmx.net>
2018-08-23 12:31:53 +0200

    completed the GameLogic (kinda)

--- src/com/company/GameLogic.java
+++ src/com/company/GameLogic.java
@@ -2,14 +2,18 @@ package com.company;
 
 public class GameLogic {
 
-    double _ThreeValue = 10.0d;
-    double _TwoValue = 2.0d;
+    private double _ThreeValue = 10.0d;
+    private double _TwoValue = 2.0d;
+    private double _BigEnemyChoice = -10.0d;
+    private double _BigOwnChoice = 10.0d;
 
 
     private Board board;
     private byte player;
     private byte enemyPlayer;
-    private boolean[][] updating_ac;    // updated allowed cells
+    private boolean[][] updatingAllowedCells;
+    private byte[][] updatingCells;
+    private byte[][] updatingFields = new byte[3][3];
 
     private double[][] values = new double[9][9];
 
@@ -33,12 +37,18 @@ public class GameLogic {
 
     private void UpdateVariables(){     // update values
         boolean[][] start_ac = board.getAllowedCells();
+        updatingAllowedCells = start_ac;
+        updatingCells = board.getCells();
+        updatingFields = board.getFields();
         for (byte x = 0; x < 9; x++){
             for (byte y = 0; y < 9; y++){
                 if (start_ac[x][y]){   // cell is allowed
-                    updating_ac = board.getAllowedCells();
-                    updating_ac[x][y] = false;
-                    values[x][y] = WinFieldPossible_getVal(x,y, player);
+                    if (board.getFields()[x%3][y%3] == this.player || board.getFields()[x%3][y%3] == this.enemyPlayer){
+                        values[x][y] = -1000.0d;
+                    }
+                    else {
+                        values[x][y] = WinFieldPossible_getVal(x,y, player);
+                    }
                 }
                 else {                  // cell isn't allowed
                     values[x][y] = -1000.0d;
@@ -51,8 +61,14 @@ public class GameLogic {
         // 1 -> x
         // 2 -> o
         int totalVal = 0;
+
+        updatingCells[start_xcoord][start_ycoord] = p;
+        byte[] nextActiveField = new byte[2];
+        nextActiveField[0] = (byte)(start_xcoord % 3);
+        nextActiveField[1] = (byte)(start_ycoord % 3);
+
         // es wird terminated, wenn die Reihe nicht auf dem originalem Brett gewonnen werden kann
-        if (board.getFields()[start_xcoord % 3][start_ycoord % 3] == 0){    // if field isnt won by anybody
+        if (updatingFields[start_xcoord % 3][start_ycoord % 3] == 0){    // if field isn't won by anybody
             if (board.checkRows(start_xcoord, start_ycoord, p)){
                 if (p == this.player){
                     totalVal += _ThreeValue;
@@ -60,6 +76,7 @@ public class GameLogic {
                 else if (p == this.enemyPlayer){
                     totalVal -= _ThreeValue;
                 }
+                updatingFields[start_xcoord % 3][start_ycoord % 3] = p;
             }
             else if (board.checkUsefulPairs(start_xcoord, start_ycoord, p)){
                 if (p == this.player){
@@ -69,15 +86,36 @@ public class GameLogic {
                     totalVal -= _TwoValue;
                 }
             }
+
+            // update updatingAllowedCells based on updatingCells, updatingFields and nextActiveField
+            updatingAllowedCells = board.checkAllowedCells(updatingCells, nextActiveField);
+
+            for (byte x = 0; x < 9; x++){
+                for (byte y = 0; y < 9; y++){
+                    if (updatingAllowedCells[x][y]){   // cell is allowed
+                        if (updatingFields[x%3][y%3] == this.player || updatingFields[x%3][y%3] == this.enemyPlayer){
+                            values[x][y] = -1000.0d;
+                    }
+                        else {
+                            values[x][y] = WinFieldPossible_getVal(x,y, player);
+                        }
+                    }
+                    else {                  // cell isn't allowed
+                        values[x][y] = -1000.0d;
+                    }
+                }
+            }
+
+            return totalVal;
         }
         else {      // if field is won by someone
             if (p == this.player){
-
+                totalVal += _BigOwnChoice;
             }
             else if (p == this.enemyPlayer){
-
+                totalVal += _BigEnemyChoice;
             }
+            return totalVal;
         }
-        return totalVal;
     }
 }
