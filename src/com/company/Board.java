9f3b723ce0871c467e5bc13776b4b487470426af
Jannick Vitak <jannvita18@computercamp.local>
2018-08-24 17:45:14 +0200

    hopefully done

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -505,8 +505,9 @@ public class Board {
     }
     public boolean[][] checkAllowedCells(byte[][] cells, byte[]activeField){
         //Search for active cells in cells[] by looking at activeField
+        boolean[][] allowedCells = new boolean[9][9];
         if (activeField[0]!=3){                    //the field isn`t won yet//this array is true for every cell the player is allowed to play
-            boolean[][] allowedCells = new boolean[9][9];
+
             for (int i1 = 0; i1<=2;i1++) {
                 for (int i2 = 0; i2 <= 2; i2++) {
                     if(cells[activeField[0] * 3 + i1][activeField[1] * 3 + i2]==0){               //checking whether cell is already occupied
